f2825cd794e312703b22c7612863153a0b7f22ad
draekkdev <rodriver1992@gmail.com>
2024-08-02 09:33:22 -0400

    working on models.user

---
+++ models/user.js
@@ -0,0 +1,11 @@
+const { DataTypes } = require("sequelize");
+const sequelize = require("./config/database");
+
+const User = sequelize.define(
+  'User',
+  {
+    name: {
+      type: 
+    }
+  }
+);
