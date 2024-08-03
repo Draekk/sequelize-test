2e1b4c88996b0729625d0ee6d13d3c84247f7814
draekkdev <rodriver1992@gmail.com>
2024-08-03 08:51:32 -0400

    wo repositories/userRepository.js | added findAll, findById, update and destroy functions

--- repositories/userRepository.js
+++ repositories/userRepository.js
@@ -7,9 +7,63 @@ async function save(user) {
       password: user.password,
     });
   } catch (error) {
-    console.log("An error has occurred:", error);
+    console.error("An error has occurred:", error);
     return null;
   }
 }
 
-module.exports = { save };
+async function findAll() {
+  try {
+    return await User.findAll();
+  } catch (error) {
+    console.error("An error has occurred:", error);
+    return null;
+  }
+}
+
+async function findById(id) {
+  try {
+    return await User.findAll({
+      where: {
+        id,
+      },
+    });
+  } catch (error) {
+    console.error("An error has occurred:", error);
+    return null;
+  }
+}
+
+async function update(user) {
+  try {
+    return await User.update(
+      {
+        username: user.username,
+        password: user.password,
+      },
+      {
+        where: {
+          id: user.id,
+        },
+      }
+    );
+  } catch (error) {
+    console.error("An error has occurred:", error);
+    return null;
+  }
+}
+
+async function destroy(id) {
+  try {
+    return await User.destroy({
+      where: {
+        id,
+      },
+    });
+  } catch (error) {
+    console.error("An error has occurred:", error);
+    return null;
+  }
+}
+
+module.exports = { save, findAll, findById, update, destroy };
