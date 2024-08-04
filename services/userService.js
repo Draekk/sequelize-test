ed95efd46d545f022b70e1ac8e53f436a9615ba9
draekkdev <rodriver1992@gmail.com>
2024-08-03 22:15:53 -0400

    wo services/userService.js | updated createUser function

--- services/userService.js
+++ services/userService.js
@@ -2,22 +2,17 @@ const repository = require("../repositories/userRepository");
 
 async function createUser(json) {
   try {
-    const user = JSON.parse(json);
-    console.log(user);
-    const response = createResponse200(200, "OK", await repository.save(user));
-    console.log(response);
-    return response;
+    const res = await repository.save(JSON.parse(json));
+    const data = {
+      id: res.id,
+      username: res.username,
+      password: res.password,
+    };
+    return JSON.stringify(data);
   } catch (error) {
     console.error("An error has occurred:", error);
     return null;
   }
-}
-
-function createResponse200(status, statusText, data) {
-  const response = new Response(data);
-  response.status = status;
-  response.statusText = statusText;
-  return response;
 }
 
 module.exports = { createUser };
